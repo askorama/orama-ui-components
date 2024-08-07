cd945c54683f35780e53bef71df0887accb7447c
Francesca Giannino <francesca.giannino86@gmail.com>
2024-08-07 13:10:42 +0200

    fix: suggestions condition

--- packages/ui-stencil/src/components/internal/orama-search-results/orama-search-results.tsx
+++ packages/ui-stencil/src/components/internal/orama-search-results/orama-search-results.tsx
@@ -38,7 +38,7 @@ export class SearchResults {
     if (!this.searchTerm) {
       return (
         <div class="suggestions-wrapper">
-          {this.suggestions?.length && (
+          {!!this.suggestions?.length && (
             <orama-text as="h2" styledAs="small" class="suggestions-title" variant="secondary">
               Suggestions
             </orama-text>
