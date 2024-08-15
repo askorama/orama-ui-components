c92a6b397f957d7dbdf637917458a9938ef9c3fe
Rodrigo José Borba Fernandes <rodrigoj.borba@gmail.com>
2024-08-15 01:13:13 -0300

    fix: SSR issue on importing CJS

--- packages/ui-stencil-react/tsup.config.ts
+++ packages/ui-stencil-react/tsup.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig, type Options } from "tsup";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 
 export default defineConfig((options): Options => {
+  const source = join("../ui-stencil/dist/orama-ui/orama-ui.css");
+  const destination = join("./src/orama-ui.css");
+
+  // Copy the file
+  copyFileSync(source, destination);
+  console.log(`File copied from ${source} to ${destination}`);
+
   return {
     entry: ["src/**/*.ts"],
     format: ["cjs", "esm"],
