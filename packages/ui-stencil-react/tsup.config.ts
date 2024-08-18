e5e27717d73331d1f5f13508609e73ea2f9de386
Rodrigo José Borba Fernandes <rodrigoj.borba@gmail.com>
2024-08-18 19:08:01 -0300

    chore: index wc componets lib into the bundle

--- packages/ui-stencil-react/tsup.config.ts
+++ packages/ui-stencil-react/tsup.config.ts
@@ -1,16 +1,6 @@
 import { defineConfig, type Options } from "tsup";
-// @ts-ignore - We need to find out why ts is complaining about node bult in deps not being found
-import { copyFileSync } from "node:fs";
-// @ts-ignore - We need to find out why ts is complaining about node bult in deps not being found
-import { join } from "node:path";
+
 export default defineConfig((options): Options => {
-  const source = join("../ui-stencil/dist/orama-ui/orama-ui.css");
-  const destination = join("./src/orama-ui.css");
-
-  // Copy the file
-  copyFileSync(source, destination);
-  console.log(`File copied from ${source} to ${destination}`);
-
   return {
     entry: ["src/index.ts", "src/orama-ui.css"],
     format: ["cjs", "esm"],
@@ -23,5 +13,6 @@ export default defineConfig((options): Options => {
     outDir: "dist",
     external: ["react", "react-dom"],
     injectStyle: true,
+    noExternal: ["@orama/wc-components"],
   };
 });
