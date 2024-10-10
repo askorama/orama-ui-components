6a653af8b48a182f5b16950e328d107ac9eb311a
Fausto Quaggia <fausto.quaggia@gmail.com>
2024-10-11 00:41:38 +0200

    feat: footer link with utm source and medium

--- packages/ui-stencil/src/components/internal/orama-footer/orama-footer.tsx
+++ packages/ui-stencil/src/components/internal/orama-footer/orama-footer.tsx
@@ -16,14 +16,30 @@ export class OramaFooter {
   @Prop() class?: string
   @Prop() colorScheme?: Omit<ColorScheme, 'system'> = 'light'
 
+  private poweredByDestinationUrl: string
+  private linkTarget = '_blank'
+
+  componentWillLoad() {
+    const utmSource = encodeURIComponent(window.location.hostname)
+    this.poweredByDestinationUrl = `https://www.orama.com/?utm_source=${utmSource}&utm_medium=powered-by`
+    if (['localhost', 'orama.com'].includes(utmSource)) {
+      this.linkTarget = '_parent'
+    }
+  }
+
   render() {
     const imgName = this.colorScheme === 'dark' ? 'orama-when-dark.svg' : 'orama-when-light.svg'
     return (
       <Host>
         <div class="powered-by">
-          <orama-text as="small">Powered by</orama-text>
-          <a href="https://www.orama.com" target="_blank" rel="noopener noreferrer" class="logo-link">
-            <img src={`https://website-assets.oramasearch.com/${imgName}`} alt="Powered by Orama" class="logo" />
+          <a href={this.poweredByDestinationUrl} target={this.linkTarget} rel="noopener noreferrer" class="logo-link">
+            <orama-text as="small">Powered by</orama-text>
+            <img
+              src={`https://website-assets.oramasearch.com/${imgName}`}
+              alt="Powered by Orama"
+              class="logo"
+              width={62}
+            />
           </a>
         </div>
       </Host>
